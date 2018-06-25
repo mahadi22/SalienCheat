b92698003dbce79cabe7c6363a616738fcc92cb6
Mahadiputra <6297984+mahadi22@users.noreply.github.com>
2018-06-25 17:05:23 +0700

    Update simplerun.bat

--- simplerun.bat
+++ simplerun.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 :start
 echo:
-SET /P token=Please input your token file with extension or your token string \n(ex: token0.txt or yourtokenstring) 
+SET /P token=Please nput token file or your token string (ex: token0.txt or yourtokenstring)  
 @title SALIEN - %token%
 if not defined token goto error
 goto runit
