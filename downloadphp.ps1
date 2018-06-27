b93635767329be883f5a03db8447e1d9f05af936
Mahadiputra <6297984+mahadi22@users.noreply.github.com>
2018-06-27 08:34:02 +0700

    Update downloadphp.ps1

--- downloadphp.ps1
+++ downloadphp.ps1
@@ -2,3 +2,4 @@
 Expand-Archive -LiteralPath php.zip -DestinationPath php\
 Copy-Item -Path php\php.ini-production -Destination php\php.ini
 ((Get-Content php\php.ini)) -Replace ";extension=curl", ("extension=" + (Get-Item -Path ".\php") + "\ext\php_curl.dll") | Set-Content php\php.ini
+((Get-Content php\php.ini)) -Replace ";date.timezone =", ("date.timezone = Asia/Jakarta") | Set-Content php\php.ini
