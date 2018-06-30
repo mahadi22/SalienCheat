d13f82dc46c8244cc6b10a0700baaa0a24033ee1
Mahadiputra <6297984+mahadi22@users.noreply.github.com>
2018-06-30 09:54:04 +0700

    Create reallysimplerun.bat

---
+++ reallysimplerun.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+:start
+echo:
+SET /P token=Please nput token file or your token string (ex: token0.txt or yourtokenstring)  
+@title SALIEN - %token%
+if not defined token goto error
+goto runit
+
+:error
+echo:
+echo     Token invalid or file not found
+echo:
+goto start
+
+:runit
+php "%~dp0cheat.php" %token% 
+goto end
+ 
+:end
+echo:
+echo     This will close in 3sec
+TIMEOUT /T 3 /NOBREAK > nul
