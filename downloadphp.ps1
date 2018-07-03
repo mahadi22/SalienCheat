78bbbcf345fa155288b4b9c38d267547d9ef8b59
Mahadiputra <6297984+mahadi22@users.noreply.github.com>
2018-07-03 07:31:58 +0700

    Update downloadphp.ps1

--- downloadphp.ps1
+++ downloadphp.ps1
@@ -1,5 +1,15 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+if($PSVersiontable.PSVersion.Major -lt 5)
+{
+    if((Test-Path php.exe -pathtype leaf) -eq $False)
+    {
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+        [IO.Compression.ZipFile]::ExtractToDirectory('php.zip', '.\')
+    }
+}
+else {
 Expand-Archive -LiteralPath php.zip -DestinationPath .\
+}
 Copy-Item -Path .\php.ini-production -Destination .\php.ini
 ((Get-Content .\php.ini)) -Replace ";extension=curl", ("extension=" + (Get-Item -Path ".\") + "\ext\php_curl.dll") | Set-Content .\php.ini
 ((Get-Content .\php.ini)) -Replace ";date.timezone =", ("date.timezone = Asia/Jakarta") | Set-Content .\php.ini
